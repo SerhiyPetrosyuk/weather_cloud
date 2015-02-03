fcdbd18d03173184cf909df8c0632573476b6723
SerhiyPetrosyuk <petrosyuk@mlsdev.com>
2015-02-03 12:06:40 +0200

    Add database tests

--- app/src/main/java/com/mlsdev/serhiy/weathercloud/data/WeatherDbHelper.java
+++ app/src/main/java/com/mlsdev/serhiy/weathercloud/data/WeatherDbHelper.java
@@ -12,7 +12,7 @@ import static com.mlsdev.serhiy.weathercloud.data.WeatherContract.*;
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
     // If you change the database schema, you must increment the database version.
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     private static final String DATABASE_NAME = "weather.db";
     
@@ -31,15 +31,15 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                 " UNIQUE (" + LocationEntry.COLUMN_LOCATION_SETTING + ") ON CONFLICT IGNORE);";
         
         final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
-                WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 // the ID of the location entry associated with this weather data
                 WeatherEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL, " +
                 WeatherEntry.COLUMN_DATETEXT + " TEXT NOT NULL, " +
                 WeatherEntry.COLUMN_SHORT_DESC + " TEXT NOT NULL, " +
                 WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL," +
 
-                WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
-                WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
+                WeatherEntry.COLUMN_MIN_TEMP + " INTEGER NOT NULL, " +
+                WeatherEntry.COLUMN_MAX_TEMP + " INTEGER NOT NULL, " +
 
                 WeatherEntry.COLUMN_HUMIDITY + " REAL NOT NULL, " +
                 WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
@@ -54,7 +54,8 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                 // per location, it's created a UNIQUE constraint with REPLACE strategy
         
                 " UNIQUE (" + WeatherEntry.COLUMN_DATETEXT + ", " + WeatherEntry.COLUMN_LOC_KEY + ") " + 
-                " ON CONFLICT REPLACE;";
+                " ON CONFLICT REPLACE " +
+                ");";
         db.execSQL(SQL_CREATE_LOCATION_TABLE);
         db.execSQL(SQL_CREATE_WEATHER_TABLE);
     }
